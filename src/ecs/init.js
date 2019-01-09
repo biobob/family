44a9fba463b011608be7b62369c1c5813bc6555a
biobob <robert.komorovsky@gmail.com>
2019-01-09 22:54:10 +0100

    Checkbox is added via JS initialization now
    [skip ci]

--- src/ecs/init.js
+++ src/ecs/init.js
@@ -1,9 +1,16 @@
 (() => {
-    document
-        .getElementById('menu')
-        .insertAdjacentHTML(
-            'beforeend',
-            '<label for="menu-flag" id="hamburger"><div></div><div></div><div></div></label>'
-        );
-    window.onscroll = () => document.querySelector('header').className = this.scrollY < 9 ? '' : 'scroll';
+    let header = document.querySelector('header');
+    document.getElementById('menu').insertAdjacentHTML(
+        'beforeend',
+        `<label for="menu-flag" id="hamburger">
+            <div></div>
+            <div></div>
+            <div></div>
+        </label>`
+    );
+    header.insertAdjacentHTML(
+        'beforebegin',
+        '<input type="checkbox" id="menu-flag">'
+    );
+    window.onscroll = () => header.className = this.scrollY < 9 ? '' : 'scroll';
 })();

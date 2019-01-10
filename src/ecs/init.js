0841a9b8af6c4dc2ae416f930b9aeafa8b04eeef
biobob <robert.komorovsky@gmail.com>
2019-01-10 09:23:10 +0100

    Sliding menu is now dynamicallt added via JS

--- src/ecs/init.js
+++ src/ecs/init.js
@@ -1,5 +1,6 @@
 (() => {
-    let header = document.querySelector('header');
+    let header = document.querySelector('header'),
+        slider = document.createElement('div');
     document.getElementById('menu').insertAdjacentHTML(
         'beforeend',
         `<label for="menu-flag" id="hamburger">
@@ -12,5 +13,8 @@
         'beforebegin',
         '<input type="checkbox" id="menu-flag">'
     );
+    slider.id = 'slider';
+    slider.append(document.querySelector('#menu > ul').cloneNode(true));
+    header.after(slider);
     window.onscroll = () => header.className = this.scrollY < 9 ? '' : 'scroll';
 })();
